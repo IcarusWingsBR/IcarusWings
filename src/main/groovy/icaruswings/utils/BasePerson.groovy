c91b77fb8c6d1f39e0d798e9f36b5d9290ac574f
JGranvile <joaoggranfer@gmail.com>
2024-05-15 17:12:13 -0300

    Cria uma BasePerson para ser usada em domains

---
+++ src/main/groovy/icaruswings/utils/BasePerson.groovy
@@ -0,0 +1,36 @@
+package icaruswings.utils
+
+abstract class BasePerson {
+    String name
+
+    String email
+
+    String cpfCnpj
+
+    String cep
+
+    String street
+
+    String neighborhood
+
+    String city
+
+    String state
+
+    Integer number
+
+    String details
+
+    static constraints = {
+        name nullable: false, blank: false
+        email nullable: false, blank: false
+        cpfCnpj nullable: false, blank: false
+        cep nullable: false, blank: false
+        street nullable: false, blank: false
+        neighborhood nullable: false, blank: false
+        city nullable: false, blank: false
+        state nullable: false, blank: false
+        number nullable: false
+        details nullable: false
+    }
+}
