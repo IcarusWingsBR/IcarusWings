92b3d65f0cb7d23aaf7c526ced37ef06cafa0810
JGranvile <joaoggranfer@gmail.com>
2024-05-16 20:04:51 -0300

    Refatora a validação de houseNumber para aumentar o escopo criando um StringUtils

--- src/main/groovy/icaruswings/utils/validations/ValidateHouseNumber.groovy
+++
@@ -1,8 +0,0 @@
-package icaruswings.utils.validations
-
-public class ValidateHouseNumber {
-
-    public static Boolean isValidHouseNumber(String number) {
-        return !number.matches(".*\\D.*");
-    }
-}
