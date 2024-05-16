51ff63de8cf13ef56b444a3094f6b9f7cb98e398
thiago <thiago.correa@asaas.com.br>
2024-05-16 14:48:16 -0300

    Altera constraints da classe Customer

--- grails-app/domain/icaruswings/Customer.groovy
+++ grails-app/domain/icaruswings/Customer.groovy
@@ -6,7 +6,7 @@ import icaruswings.utils.BasePerson
 class Customer extends BasePerson{
 
     static constraints = {
-        email nullable: false, blank: false, email: true, unique: true
-        cpfCnpj nullable: false, blank: false, size: 11..18, unique: true
+        email unique: true
+        cpfCnpj size: 11..18, unique: true
     }
 }
