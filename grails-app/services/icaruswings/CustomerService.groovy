7945d2b894fa690fc2e6bb8538ce00ba7f3872a8
JGranvile <joaoggranfer@gmail.com>
2024-05-17 15:21:50 -0300

    Adiciona as lógicas de valicação ao Customer

--- grails-app/services/icaruswings/CustomerService.groovy
+++ grails-app/services/icaruswings/CustomerService.groovy
@@ -1,18 +1,36 @@
 package icaruswings
 
 import grails.gorm.transactions.Transactional
+import grails.validation.ValidationException
 import icaruswings.utils.PersonType
+import icaruswings.utils.validations.ValidateCpfCnpj
+import icaruswings.utils.validations.ValidateName
+import icaruswings.utils.validations.ValidateEmail
 
 @Transactional
 class CustomerService {
+
     public Customer save(Map parsedParams) {
+
+        Customer validateCustomer = validateCustomerParams(parsedParams)
+
+        if (validateCustomer.hasErrors()) {
+            throw new ValidationException("Não foi possível salvar o cliente", validateCustomer.errors)
+        }
+
         Customer customer = new Customer()
 
         customer.name = parsedParams.name
 
         customer.email = parsedParams.email
 
-        customer.cpfCnpj = parsedParams.cpfCnpj
+        if(ValidateCpfCnpj.isCPF(parsedParams.cpfCnpj)) {
+            customer.cpfCnpj = ValidateCpfCnpj.cleanCpfCnpj(parsedParams.cpfCnpj)
+            customer.personType = PersonType.NATURAL
+        } else if (ValidateCpfCnpj.isCNPJ(parsedParams.cpfCnpj)) {
+            customer.cpfCnpj = ValidateCpfCnpj.cleanCpfCnpj(parsedParams.cpfCnpj)
+            customer.personType = PersonType.LEGAL
+        }
 
         customer.cep = parsedParams.cep
 
@@ -31,6 +49,30 @@ class CustomerService {
         customer.personType = PersonType.NATURAL
 
         customer.save(failOnError: true)
+
+        return customer
+    }
+
+    private Customer validateCustomerParams(Map parsedParams) {
+        Customer customer = new Customer()
+
+        if(!parsedParams.name) {
+            customer.errors.rejectValue("name",  null,"O campo nome é obrigatório")
+        } else if (!ValidateName.isValidName(parsedParams.name)) {
+            customer.errors.rejectValue("name", null, "O nome informado é inválido")
+        }
+
+        if(!parsedParams.email) {
+            customer.errors.rejectValue("email", null, "O campo email é obrigatório")
+        } else if(!ValidateEmail.isValidEmail(parsedParams.email)){
+            customer.errors.rejectValue("email", null, "O email informado é inválido")
+        }
+
+        if(!parsedParams.cpfCnpj) {
+            customer.errors.rejectValue("cpfCnpj", null, "O campo Cpf/Cnpj é obrigatório")
+        } else if (!ValidateCpfCnpj.isCPF(parsedParams.cpfCnpj) && !ValidateCpfCnpj.isCNPJ(parsedParams.cpfCnpj)) {
+            customer.errors.rejectValue("cpfCnpj", null, "O campo Cpf/Cnpj está inválido")
+        }
 
         return customer
     }
