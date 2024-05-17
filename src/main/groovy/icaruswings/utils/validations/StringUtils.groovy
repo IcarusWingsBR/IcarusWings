2e2fdad6e41d30387fe1e75f747c5e0e5f7c9da5
thiago <thiago.correa@asaas.com.br>
2024-05-17 18:35:29 -0300

    refatora função que limpa cpf/cnpj para que limpe qualquer String

--- src/main/groovy/icaruswings/utils/validations/StringUtils.groovy
+++ src/main/groovy/icaruswings/utils/validations/StringUtils.groovy
@@ -5,4 +5,10 @@ public class StringUtils {
     public static Boolean containsOnlyNumbers(String number) {
         return !number.matches(".*\\D.*");
     }
+
+    public static String cleanString(String str) {
+        String sanitizedStr = str.replaceAll("[^0-9]", "")
+
+        return sanitizedStr
+    }
 }
