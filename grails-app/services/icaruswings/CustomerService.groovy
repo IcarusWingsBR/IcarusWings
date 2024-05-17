330162aeef66f7295c47820186a4fd0dda34898d
thiago <thiago.correa@asaas.com.br>
2024-05-17 19:38:40 -0300

    Adiciona validações de endereço faltantes

--- grails-app/services/icaruswings/CustomerService.groovy
+++ grails-app/services/icaruswings/CustomerService.groovy
@@ -4,7 +4,7 @@ import grails.gorm.transactions.Transactional
 import grails.validation.ValidationException
 import icaruswings.utils.PersonType
 import icaruswings.utils.validations.ValidateCpfCnpj
-import icaruswings.utils.validations.ValidateName
+import icaruswings.utils.validations.StringUtils
 import icaruswings.utils.validations.ValidateEmail
 
 @Transactional
@@ -24,11 +24,11 @@ class CustomerService {
 
         customer.email = parsedParams.email
 
+        customer.cpfCnpj = ValidateCpfCnpj.cleanCpfCnpj(parsedParams.cpfCnpj)
+
         if(ValidateCpfCnpj.isCPF(parsedParams.cpfCnpj)) {
-            customer.cpfCnpj = ValidateCpfCnpj.cleanCpfCnpj(parsedParams.cpfCnpj)
             customer.personType = PersonType.NATURAL
         } else if (ValidateCpfCnpj.isCNPJ(parsedParams.cpfCnpj)) {
-            customer.cpfCnpj = ValidateCpfCnpj.cleanCpfCnpj(parsedParams.cpfCnpj)
             customer.personType = PersonType.LEGAL
         }
 
@@ -70,6 +70,42 @@ class CustomerService {
             customer.errors.rejectValue("cpfCnpj", null, "O campo Cpf/Cnpj é obrigatório")
         } else if (!ValidateCpfCnpj.isCPF(parsedParams.cpfCnpj) && !ValidateCpfCnpj.isCNPJ(parsedParams.cpfCnpj)) {
             customer.errors.rejectValue("cpfCnpj", null, "O campo Cpf/Cnpj está inválido")
+        }
+
+        if(!parsedParams.cep) {
+            payer.errors.rejectValue("cep", null, "O campo cep é obrigatório")
+        } else if (!StringUtils.containsOnlyNumbers(parsedParams.cep)) {
+            payer.errors.rejectValue("cep", null, "O cep inserido é inválido")
+        }
+
+        if(!parsedParams.street) {
+            payer.errors.rejectValue("street", null, "O campo rua é obrigatório")
+        }
+
+        if(!parsedParams.neighborhood) {
+            payer.errors.rejectValue("neighborhood", null, "O campo bairro é obrigatório")
+        }
+
+        if(!parsedParams.number) {
+            payer.errors.rejectValue("number", null, "O campo número de residência é obrigatório")
+        } else if (!StringUtils.containsOnlyNumbers(parsedParams.number)) {
+            payer.errors.rejectValue("number", null, "O número de residência é inválido")
+        }
+
+        if(!parsedParams.complement) {
+            payer.errors.rejectValue("complement", null, "O campo complemento é obrigatório")
+        }
+
+        if(!parsedParams.city) {
+            payer.errors.rejectValue("city", null, "O campo cidade é obrigatório")
+        } else if (!StringUtils.isValidString(parsedParams.city)) {
+            payer.errors.rejectValue("city", null, "A cidade informado é inválida")
+        }
+
+        if(!parsedParams.state) {
+            payer.errors.rejectValue("state", null, "O campo estado é obrigatório")
+        } else if (!StringUtils.isValidString(parsedParams.state)) {
+            payer.errors.rejectValue("state", null, "O estado informado é inválido")
         }
 
         return customer
