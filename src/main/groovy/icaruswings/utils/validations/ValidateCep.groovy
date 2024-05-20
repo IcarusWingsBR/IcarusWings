911446dc2c9849b759e476f30969ba65687239d5
thiago <thiago.correa@asaas.com.br>
2024-05-20 17:07:49 -0300

    Adiciona lógica que verifica se o CEP é somente número

--- src/main/groovy/icaruswings/utils/validations/ValidateCep.groovy
+++ src/main/groovy/icaruswings/utils/validations/ValidateCep.groovy
@@ -5,6 +5,14 @@ import groovyx.net.http.RESTClient
 class ValidateCep {
 
     public static Boolean isValidCep(String cep) {
+        if(!StringUtils.containsOnlyNumbers(cep)) return false
+
+        if(!ValidateExistingCep(cep)) return false
+
+        return true
+    }
+
+    public static Boolean ValidateExistingCep(String cep) {
         def apiUrl = "https://viacep.com.br/ws/${cep}/json/"
         def restClient = new RESTClient(apiUrl)
         def response
@@ -17,7 +25,9 @@ class ValidateCep {
             return false
         }
 
-        if (response.status != 200) return false
+        if(response.status != 200) return false
+        
+        if(response.data.erro) return false
 
         return true
     }
