192d4de1cb0463c2832f27eab581217ebd1870ea
JGranvile <joaoggranfer@gmail.com>
2024-05-21 23:23:39 -0300

    Adiciona a lógica do Repository para o Payer

---
+++ src/main/groovy/icaruswings/utils/repositories/PayerRepository.groovy
@@ -0,0 +1,21 @@
+package icaruswings.utils.repositories
+
+import icaruswings.Payer
+import org.grails.datastore.mapping.query.api.BuildableCriteria
+
+class PayerRepository implements Repository<Payer, PayerRepository> {
+    @Override
+    void buildCriteria() {
+    }
+
+    @Override
+    List<String> listAllowedFilters() {
+        return null
+    }
+    @Override
+    BuildableCriteria getBuildableCriteria() {
+        return null
+    }
+
+
+}
