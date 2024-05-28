e598145087fe54ecd62f358e36722cd422ff2ca4
thiago <thiago.correa@asaas.com.br>
2024-05-28 15:32:43 -0300

    refatora validação e parse de data

--- src/main/groovy/icaruswings/utils/date/DateUtils.groovy
+++ src/main/groovy/icaruswings/utils/date/DateUtils.groovy
@@ -3,8 +3,24 @@ package icaruswings.utils.date
 import java.sql.Date
 import java.text.SimpleDateFormat
 import java.text.ParseException
+    
+public class DateUtils {
+    
+    public static Boolean isBeforeToday(String data) {
+        Date parsedData = parse(data)
+        
+        return isBeforeToday(parsedData)
+    }
 
-class DateParser {
+    public static Boolean isBeforeToday(Date date) {
+        Date receivedDate = new Date(date.getTime())
+        Date today = new Date(System.currentTimeMillis());
+        
+        if (receivedDate.before(today)) return false
+
+        return true
+    }
+
     public static Date parse(String dateString) {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy")
