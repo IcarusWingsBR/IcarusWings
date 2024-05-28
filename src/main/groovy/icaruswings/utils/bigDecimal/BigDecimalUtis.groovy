0fc25621055f36a47d5b7bee3a2d72ca2cbd1933
thiago <thiago.correa@asaas.com.br>
2024-05-28 18:27:43 -0300

    remove funções que não serão usadas

--- src/main/groovy/icaruswings/utils/bigDecimal/BigDecimalUtis.groovy
+++ src/main/groovy/icaruswings/utils/bigDecimal/BigDecimalUtis.groovy
@@ -5,27 +5,6 @@ import java.util.regex.Pattern
 
 class BigDecimalUtis{
 
-    public static Boolean isValidBigDecimal(String stringValue) {
-        if (!isFormatValid(stringValue)) return false
-
-        BigDecimal value = parse(stringValue)
-
-        if (value < 0) return false
-
-        return true
-    }
-
-    private static Boolean isFormatValid(String stringValue) {
-        String regex = "^\\d+(,\\d+)?(?<!,)"
-
-        Pattern pattern = Pattern.compile(regex)
-        Matcher matcher = pattern.matcher(stringValue)
-
-        if (!matcher.matches()) return false
-
-        return true
-    }
-
     public static BigDecimal parse(String stringValue) {
         String valueWithDote = stringValue.replaceAll( "," , "." )
         BigDecimal value = new BigDecimal(valueWithDote)
