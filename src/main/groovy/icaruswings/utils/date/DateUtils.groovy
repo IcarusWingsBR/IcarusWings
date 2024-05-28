6c14a30e8915d45aaa8e6538742f4bbc16792b03
thiago <thiago.correa@asaas.com.br>
2024-05-28 15:47:25 -0300

    atualiza tipo de data para java.util.Date

--- src/main/groovy/icaruswings/utils/date/DateUtils.groovy
+++ src/main/groovy/icaruswings/utils/date/DateUtils.groovy
@@ -1,31 +1,32 @@
 package icaruswings.utils.date
 
-import java.sql.Date
+import java.util.Date
 import java.text.SimpleDateFormat
 import java.text.ParseException
     
 public class DateUtils {
     
-    public static Boolean isBeforeToday(String data) {
-        Date parsedData = parse(data)
+    public static Boolean isBeforeToday(String date) {
+        Date parsedDate = parse(date)
         
-        return isBeforeToday(parsedData)
+        return isBeforeToday(parsedDate)
     }
 
     public static Boolean isBeforeToday(Date date) {
         Date receivedDate = new Date(date.getTime())
         Date today = new Date(System.currentTimeMillis());
         
-        if (receivedDate.before(today)) return false
+        if (receivedDate.before(today)) return true
 
-        return true
+        return false
     }
 
     public static Date parse(String dateString) {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy")
+            Date parsedDate = simpleDateFormat.parse(dateString)
 
-            return new Date(simpleDateFormat.parse(dateString).getTime())
+            return parsedDate
         } catch (ParseException parseException) {
             throw new ParseException("Formato de data inválido: " + parseException)
         } 
