3c4d39eb8e033481ed4bd7aecc5d93ceb5c4bca4
thiago <thiago.correa@asaas.com.br>
2024-05-29 16:36:20 -0300

    altera mensagem do log

--- grails-app/controllers/icaruswings/BaseController.groovy
+++ grails-app/controllers/icaruswings/BaseController.groovy
@@ -5,8 +5,8 @@ import grails.validation.ValidationException
 abstract class BaseController {
 
     def handleValidationExceptionException(ValidationException exception) {
-        log.error("CustomerController.save >> Erro ao criar customer ${params}", exception)
-        
+        log.error("Erro com os seguintes parâmetros ${params}", exception)
+
         flash.type = "error"
         flash.message = "Verifique os dados informados e tente novamente."
 
