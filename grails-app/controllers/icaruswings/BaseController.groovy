d8cf077b1607e83c237c951e827fc007c2be40c7
thiago <thiago.correa@asaas.com.br>
2024-05-31 17:20:18 -0300

    refatora base controller

--- grails-app/controllers/icaruswings/BaseController.groovy
+++ grails-app/controllers/icaruswings/BaseController.groovy
@@ -5,33 +5,19 @@ import grails.validation.ValidationException
 abstract class BaseController {
 
     def handleValidationExceptionException(ValidationException exception) {
+        String message =  exception.getErrors().getAllErrors()
+            .collect { it.getDefaultMessage() }
+            .join("\n")
+
         log.error("Erro com os seguintes parâmetros ${params}", exception)
 
         flash.type = "error"
-        flash.message = "Verifique os dados informados e tente novamente."
+        flash.message = message
 
         redirect(action: "index", params: params)
     }
 
-    def handleNumberFormatException(NumberFormatException exception) {
-        log.error("Erro com os seguintes parâmetros ${params}", exception)
-
-        flash.type = "error"
-        flash.message = "Verifique se você preencheu todos os campos e tente novamente."
-
-        redirect(action: "index", params: params)
-    }
-
-    def handleNullPointerException(NullPointerException exception) {
-        log.error("Erro com os seguintes parâmetros ${params}", exception)
-
-        flash.type = "error"
-        flash.message = "Por favor, preencha todos os campos."
-
-        redirect(action: "index", params: params)
-    }
-
-     def handleRuntimeException(RuntimeException exception) {
+    def handleException(Exception exception) {
         log.error("Erro com os seguintes parâmetros ${params}", exception)
 
         flash.type = "error"
