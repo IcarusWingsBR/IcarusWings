841fcada3028dd13b37d37f7f3dfed1fe2ad7555
thiago <thiago.correa@asaas.com.br>
2024-06-05 14:11:12 -0300

    faz o envio dos emails serem assíncronos

--- grails-app/services/icaruswings/EmailService.groovy
+++ grails-app/services/icaruswings/EmailService.groovy
@@ -2,6 +2,7 @@ package icaruswings
 
 import grails.gorm.transactions.Transactional
 import java.text.SimpleDateFormat
+import static grails.async.Promises.*
 
 @Transactional
 class EmailService {
@@ -12,18 +13,22 @@ class EmailService {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy")
         String formattedDueDate = dateFormat.format(payment.dueDate)
 
-        mailService.sendMail {
-            to payer.email
-            subject "Criação de cobrança"
-            body "Olá ${payer.name},\nUma cobrança com valor de R\$${payment.value} foi criada e tem você como pagador.\nPor favor, se atente a data de validade: ${formattedDueDate}.\n\nAtenciosamente,\nEquipe do Icarus Wings."
+        task {
+            mailService.sendMail {
+                to payer.email
+                subject "Criação de cobrança"
+                body "Olá ${payer.name},\nUma cobrança com valor de R\$${payment.value} foi criada e tem você como pagador.\nPor favor, se atente a data de validade: ${formattedDueDate}.\n\nAtenciosamente,\nEquipe do Icarus Wings."
+            }
         }
     }
 
     public void sendStatusChangeEmailToPayer(Payer payer, Payment payment) {
-        mailService.sendMail {
-            to payer.email
-            subject "Status da sua cobrança foi alterado"
-            body "Olá ${payer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nIcarus Wings."
+        task {
+            mailService.sendMail {
+                to payer.email
+                subject "Status da sua cobrança foi alterado"
+                body "Olá ${payer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nIcarus Wings."
+            }
         }
     }
 
@@ -32,19 +37,24 @@ class EmailService {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy")
         String formattedDueDate = dateFormat.format(payment.dueDate)
 
-        mailService.sendMail {
-            to customer.email
-            subject "Criação de cobrança"
-            body "Olá ${customer.name},\nSua cobrança com valor de R\$${payment.value} foi criada e o pagador é ${payer.name}.\n\nAtenciosamente,\nEquipe do Icarus Wings."
+        task {
+            mailService.sendMail {
+                to customer.email
+                subject "Criação de cobrança"
+                body "Olá ${customer.name},\nSua cobrança com valor de R\$${payment.value} foi criada e o pagador é ${payer.name}.\n\nAtenciosamente,\nEquipe do Icarus Wings."
+            }
         }
     }
 
     public void sendStatusChangeEmailToCustomer(Payer payer, Payment payment) {
         Customer customer = payer.customer
-        mailService.sendMail {
-            to customer.email
-            subject "Status da sua cobrança foi alterado"
-            body "Olá ${customer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nIcarus Wings."
+        
+        task {
+            mailService.sendMail {
+                to customer.email
+                subject "Status da sua cobrança foi alterado"
+                body "Olá ${customer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nIcarus Wings."
+            }
         }
     }
 }
