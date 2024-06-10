a807e80e372c308c8e4b2c1328fa77adc4564a29
thiago <thiago.correa@asaas.com.br>
2024-06-10 16:10:46 -0300

    refatora as validações de determinadas strings

--- src/main/groovy/icaruswings/utils/string/StringUtils.groovy
+++ src/main/groovy/icaruswings/utils/string/StringUtils.groovy
@@ -9,22 +9,8 @@ public class StringUtils {
         return !number.matches(".*\\D.*");
     }
 
-    public static Boolean isValidString(String str) {
-        if(!isValidStringLength(str)) return false
-
-        if(!dontHaveNumberAndSpace(str)) return false
-
-        return true;
-    }
-
-    private static Boolean isValidStringLength(String str) {
-        if(str.length() < 3 || str.length() > 255) return false
-
-        return true
-    }
-
-    private static Boolean dontHaveNumberAndSpace(String str) {
-        String regex = "(?!^\\s)[[ ]|\\p{L}*]+";
+    private static Boolean dontHaveNumber(String str) {
+        String regex = "^[^0-9]*";
 
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(str);
