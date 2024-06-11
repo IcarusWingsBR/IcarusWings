9cc85008a4b66f294218c81fddf6663059fece5d
thiago <thiago.correa@asaas.com.br>
2024-06-11 13:38:01 -0300

    refatora formato das urls

--- grails-app/controllers/icaruswings/UrlMappings.groovy
+++ grails-app/controllers/icaruswings/UrlMappings.groovy
@@ -3,7 +3,7 @@ package icaruswings
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+        "/$controller/$id?/$action?(.$format)?"{
             constraints {
                 // apply constraints here
             }
@@ -12,9 +12,9 @@ class UrlMappings {
         group "/customer", {
             "/"(controller: "customer", action: [GET: "index"])
 
-            "/save"(controller: "customer", action: [POST: "save"])
+            "/"(controller: "customer", action: [POST: "save"])
 
-            "/show/$id"(controller: "customer", action: [GET: "show"])
+            "/$id/show"(controller: "customer", action: [GET: "show"])
 
             "/update"(controller: "customer", action: [POST: "update"])
 
@@ -24,15 +24,15 @@ class UrlMappings {
         group "/payer", {
             "/"(controller: "payer", action: [GET: "index"])
 
-            "/save"(controller: "payer", action: [POST: "save"])
+            "/"(controller: "payer", action: [POST: "save"])
 
-            "/show/$id"(controller: "payer", action: [GET: "show"])
+            "/$id/show"(controller: "payer", action: [GET: "show"])
 
             "/update"(controller: "payer", action: [POST: "update"])
 
-            "/delete/$id"(controller: "payer", action: [POST: "deleted", DELETE: "delete"])
+            "/$id/delete"(controller: "payer", action: [POST: "deleted", DELETE: "delete"])
 
-            "/restore/$id"(controller: "payer", action: [POST: "restore"])
+            "/$id/restore"(controller: "payer", action: [POST: "restore"])
 
             "/list"(controller: "payer", action: [GET: "list"])
 
@@ -42,25 +42,25 @@ class UrlMappings {
         group "/payment", {
             "/"(controller: "payment", action: [GET: "index"])
 
-            "/save"(controller: "payment", action: [POST: "save"])
+            "/"(controller: "payment", action: [POST: "save"])
 
-            "/show/$id"(controller: "payment", action: [GET: "show"])
+            "/$id/show"(controller: "payment", action: [GET: "show"])
 
             "/update"(controller: "payment", action: [POST: "update"])
 
-            "/delete/$id"(controller: "payment", action: [POST: "deleted", DELETE: "delete"])
+            "/$id/delete"(controller: "payment", action: [POST: "deleted", DELETE: "delete"])
 
-            "/restore/$id"(controller: "payment", action: [POST: "restore"])
+            "/$id/restore"(controller: "payment", action: [POST: "restore"])
 
             "/list"(controller: "payment", action: [GET: "list"])
 
             "/deletedList"(controller: "payment", action: [GET: "deletedList"])
 
-            "/confirmPaymentReceived/$id"(controller: "payment", action: [POST: "confirmPaymentReceived"])
+            "/$id/confirmPaymentReceived"(controller: "payment", action: [POST: "confirmPaymentReceived"])
         }
 
         group "/receipt", {
-            "/show/$id"(controller: "receipt", action: [GET: "show"])
+            "/$id/show"(controller: "receipt", action: [GET: "show"])
         }
 
         "/"(view:"/index")
