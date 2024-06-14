ae15b7cbc4b2cc0aa38a1541c5c97297e959c7b4
thiago <thiago.correa@asaas.com.br>
2024-06-14 17:49:06 -0300

    adiciona url base

--- grails-app/services/icaruswings/CustomerNotificationService.groovy
+++ grails-app/services/icaruswings/CustomerNotificationService.groovy
@@ -5,17 +5,21 @@ import grails.compiler.GrailsCompileStatic
 import icaruswings.payment.Payment
 import icaruswings.notification.CustomerNotification
 import icaruswings.notification.CustomerNotificationType
+import org.springframework.beans.factory.annotation.Value
 
 @GrailsCompileStatic
 @Transactional
 class CustomerNotificationService {
+
+    @Value('${baseUrl}')
+    String baseUrl
 
     public void savePaymentCreatedNotification(Payment payment) {
         CustomerNotification customerNotification = new CustomerNotification()
 
         customerNotification.title = "Criação de cobrança."
         customerNotification.message = "Sua cobrança de número ${payment.id} foi criada com sucesso."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_CREATED
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
@@ -26,7 +30,7 @@ class CustomerNotificationService {
 
         customerNotification.title = "Cobrança Paga."
         customerNotification.message = "Sua cobrança de número ${payment.id} foi paga por ${payment.payer.name}."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_PAYED
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
@@ -37,7 +41,7 @@ class CustomerNotificationService {
 
         customerNotification.title = "Cobrança Vencida."
         customerNotification.message = "Sua cobrança de número ${payment.id} está vencida."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_OVERDUE
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
@@ -48,7 +52,7 @@ class CustomerNotificationService {
 
         customerNotification.title = "Cobrança Excluída."
         customerNotification.message = "Sua cobrança de número ${payment.id} foi excluída com sucesso."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_DELETED
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
@@ -59,7 +63,7 @@ class CustomerNotificationService {
 
         customerNotification.title = "Cobrança Atualizada."
         customerNotification.message = "Sua cobrança de número ${payment.id} foi atualizada com sucesso."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_UPDATED
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
@@ -70,7 +74,7 @@ class CustomerNotificationService {
 
         customerNotification.title = "Cobrança Restaurada."
         customerNotification.message = "Sua cobrança de número ${payment.id} foi restaurada com sucesso."
-        customerNotification.url = "http://localhost:8080/payment/${payment.id}/show"
+        customerNotification.url = baseUrl + "payment/${payment.id}/show"
         customerNotification.type = CustomerNotificationType.PAYMENT_RESTORED
         customerNotification.customer = payment.payer.customer
         customerNotification.save(failOnError: true)
