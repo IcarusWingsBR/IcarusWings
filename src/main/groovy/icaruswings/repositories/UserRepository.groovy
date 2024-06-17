8c421687ef5d44125e98226f384173148f249a76
JGranvile <joaoggranfer@gmail.com>
2024-06-17 19:00:29 -0300

    Cria o repository para o user

---
+++ src/main/groovy/icaruswings/repositories/UserRepository.groovy
@@ -0,0 +1,33 @@
+package icaruswings.repositories
+
+import icaruswings.User
+import org.grails.datastore.mapping.query.api.BuildableCriteria
+
+class UserRepository implements Repository<User, UserRepository> {
+    @Override
+    void buildCriteria() {
+        addCriteria {
+            if (search.containsKey("id")) {
+                eq("id", Long.valueOf(search.id.toString()))
+            }
+
+            if (search.containsKey("customerId")) {
+                eq("customer.id", Long.valueOf(search.customerId.toString()))
+            }
+        }
+    }
+
+    @Override
+    List<String> listAllowedFilters() {
+        return [
+                "id",
+                "customerId"
+        ]
+    }
+
+    @Override
+    BuildableCriteria getBuildableCriteria() {
+        return User.createCriteria()
+    }
+
+}
